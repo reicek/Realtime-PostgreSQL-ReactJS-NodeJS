12759d45e983fd6cdf492aa96e894e285fd5ad4e
reicek <reicek@gmail.com>
2016-09-25 19:07:32 -0400

    v1.0.2 - added server response when adding new files

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ var update           = function(request, res, next) {
       } else {
          console.log(response),
          pgClient.query('NOTIFY "changes"');
+         res.send('200 OK');
       }
    });
 // console.log(object)
