b4d1559526178e9738da0d155495dd738df118f9
reicek <reicek@gmail.com>
2016-09-26 12:55:22 -0400

    v1.0.2 - License update

--- public/jsx/reactApp.jsx
+++ public/jsx/reactApp.jsx
@@ -1,8 +1,8 @@
 /**
  * @name    reactApp.jsx - NodeJS Restful API for PostgreSQL
- * @author  Original work Copyright (c) 2015 Cesar Anton Dorantes @reicek, for Platzi.com/blog
- *          Modified work Copyright (c) 2016 Cesar Anton Dorantes @reicek
+ * @author  Original work by Cesar Anton Dorantes @reicek, for Platzi.com/blog
  * @license 
+ * Copyright (c) 2015-2016 Cesar Anton Dorantes
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
  * and associated documentation files (the "Software"), to deal in the Software without restriction, 
  * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
